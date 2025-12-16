88160a7d02e3f2da04ac06b618ffcfd3a999173d
Francisco <github.com.visa669@passinbox.com>
2025-12-16 20:36:31 +0100

    Add layout

--- app/app.vue
+++ app/app.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <Button @click="toggleTheme">Toggle theme</Button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-const colorMode = useColorMode();
-
-function toggleTheme(): void {
-  colorMode.preference = colorMode.preference === 'light' ? 'dark' : 'light';
-}
+useSeoMeta({
+  title: 'Dev Overflow',
+  description:
+    'A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.',
+});
 </script>
---
+++ app/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-light-850 dark:bg-dark-100 relative">
+    <Head>
+      <Link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+      />
+    </Head>
+    <div class="flex">
+      <main
+        class="flex min-h-screen flex-1 flex-col px-6 pt-36 pb-6 max-md:pb-14 sm:px-14"
+      >
+        <div class="mx-auto w-full max-w-5xl">
+          <slot />
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
