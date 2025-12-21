6fff589f1282353ef73dc98cabe84105983d0d6d
Francisco <github.com.visa669@passinbox.com>
2025-12-21 19:17:10 +0100

    Add tag variant

--- app/components/ui/badge/index.ts
+++ app/components/ui/badge/index.ts
@@ -16,6 +16,7 @@ export const badgeVariants = cva(
           'border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:
           'text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground',
+        tag: 'border-transparent bg-primary text-primary-foreground [a&]:hover:bg-primary/90 subtitle-medium bg-light-800 dark:bg-dark-300 text-light-400 dark:text-light-500 rounded-md border-none px-4 py-2 uppercase',
       },
     },
     defaultVariants: {
