1bf9f874fc02543825d1a2190755417ff88d6c77
Francisco <github.com.visa669@passinbox.com>
2025-12-21 19:25:20 +0100

    Search local

---
+++ app/components/search/local.vue
@@ -0,0 +1,42 @@
+<template>
+  <InputGroup>
+    <InputGroupInput
+      v-model="searchQuery"
+      placeholder="Search..."
+      class="paragraph-regular placeholder text-dark-400 dark:text-light-700 shadow-none"
+    />
+    <InputGroupAddon>
+      <Search />
+    </InputGroupAddon>
+  </InputGroup>
+</template>
+
+<script setup lang="ts">
+import { watchDebounced } from '@vueuse/core';
+import { Search } from 'lucide-vue-next';
+import { ROUTES } from '@/utils/constants/routes';
+
+const route = useRoute();
+const router = useRouter();
+
+const searchQuery = ref<string>((route.query.query as string) ?? '');
+
+watchDebounced(
+  searchQuery,
+  (newSearchQuery) => {
+    const queryParams = route.query;
+
+    if (newSearchQuery) {
+      router.replace({
+        name: ROUTES.home,
+        query: { ...queryParams, query: searchQuery.value },
+        force: true,
+      });
+    } else {
+      delete queryParams.query;
+      router.replace({ name: ROUTES.home, query: queryParams, force: true });
+    }
+  },
+  { debounce: 300 },
+);
+</script>
