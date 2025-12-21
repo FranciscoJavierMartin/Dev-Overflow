ff9bd0963dc6f77f30886caca6b4ec934c45f1d7
Francisco <github.com.visa669@passinbox.com>
2025-12-21 19:39:21 +0100

    Add props

--- app/components/question/editor.vue
+++ app/components/question/editor.vue
@@ -1,3 +1,21 @@
 <template>
   <div></div>
 </template>
+
+<script setup lang="ts">
+const text = ref('');
+
+defineProps<{
+  id: string;
+  name: string;
+  placeholder: string;
+}>();
+
+const emit = defineEmits<{
+  (e: 'input', text: string): void;
+}>();
+
+watch(text, (newValue) => {
+  emit('input', newValue);
+});
+</script>
