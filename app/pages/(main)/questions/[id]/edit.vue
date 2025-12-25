6205ba666d6ecfbecdf24c09d11ef401347b3564
Francisco <github.com.visa669@passinbox.com>
2025-12-25 21:17:07 +0100

    Get data

--- app/pages/(main)/questions/[id]/edit.vue
+++ app/pages/(main)/questions/[id]/edit.vue
@@ -1,9 +1,21 @@
 <template>
-  <div></div>
+  <div>
+    <!-- <QuestionForm :question edit /> -->
+    <h1>{{ question?.id }}</h1>
+  </div>
 </template>
 
 <script setup lang="ts">
+import type { Question, Tag } from '@/generated/prisma/client';
+
 definePageMeta({
   middleware: ['auth'],
 });
+
+const route = useRoute();
+
+const { data: question } = await useFetch<Question & { tags: Tag[] }>(
+  `/api/questions/${route.params.id}`,
+  {},
+);
 </script>
