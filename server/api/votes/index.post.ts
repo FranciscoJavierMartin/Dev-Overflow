e576ac84a37a045fb8b289b2b14b8032d22164f7
Francisco <github.com.visa669@passinbox.com>
2025-12-28 19:58:30 +0100

    Fix vote increment when update

--- server/api/votes/index.post.ts
+++ server/api/votes/index.post.ts
@@ -10,6 +10,15 @@ export default defineEventHandler(async (event) => {
   const user: User = event.context.user;
 
   await prisma.$transaction(async (tx) => {
+    const isUpdate = !!(await tx.vote.findUnique({
+      where: {
+        targetId_authorId: {
+          targetId,
+          authorId: user.id,
+        },
+      },
+    }));
+
     await tx.vote.upsert({
       where: {
         targetId_authorId: {
@@ -27,6 +36,36 @@ export default defineEventHandler(async (event) => {
         type,
       },
     });
+
+    if (targetType === 'answer') {
+      await tx.answer.update({
+        where: {
+          id: targetId,
+        },
+        data: {
+          downvotes: {
+            increment: type === 'downvote' ? 1 : isUpdate ? -1 : 0,
+          },
+          upvotes: {
+            increment: type === 'upvote' ? 1 : isUpdate ? -1 : 0,
+          },
+        },
+      });
+    } else {
+      await tx.question.update({
+        where: {
+          id: targetId,
+        },
+        data: {
+          downvotes: {
+            increment: type === 'downvote' ? 1 : isUpdate ? -1 : 0,
+          },
+          upvotes: {
+            increment: type === 'upvote' ? 1 : isUpdate ? -1 : 0,
+          },
+        },
+      });
+    }
   });
 
   return {
