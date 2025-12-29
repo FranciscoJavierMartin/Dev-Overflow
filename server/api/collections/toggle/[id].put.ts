39b5ce35b6a4181f07ddf077e214991e2f4db6cf
Francisco <github.com.visa669@passinbox.com>
2025-12-29 11:12:34 +0100

    Toggle collection

--- server/api/collections/toggle/[id].put.ts
+++ server/api/collections/toggle/[id].put.ts
@@ -1,8 +1,42 @@
 import type { User } from '@/generated/prisma/client';
+import { prisma } from '~~/lib/prisma';
 import { questionIdSchema } from '~~/shared/utils/validations/schemas/question';
 
 export default defineEventHandler(async (event) => {
-  const { id } = await validateRouterParams(event, questionIdSchema);
+  const { id: questionId } = await validateRouterParams(
+    event,
+    questionIdSchema,
+  );
   const user: User = event.context.user;
-  return 'Hello collections/toggle';
+
+  const isInCollection = await prisma.collection.findUnique({
+    where: {
+      questionId_authorId: {
+        authorId: user.id,
+        questionId,
+      },
+    },
+  });
+
+  if (isInCollection) {
+    await prisma.collection.delete({
+      where: {
+        questionId_authorId: {
+          authorId: user.id,
+          questionId,
+        },
+      },
+    });
+  } else {
+    await prisma.collection.create({
+      data: {
+        authorId: user.id,
+        questionId,
+      },
+    });
+  }
+
+  return {
+    success: true,
+  };
 });
